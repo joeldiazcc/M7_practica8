3d69f9488c88eee7861c5ae63e1f779d40fc381f
Ivan Petrov <ipetrov20@jaumebalmes.net>
2022-11-24 20:05:54 +0100

    Inserts de ejemplo

--- import.sql
+++ import.sql
@@ -9,3 +9,8 @@ premios INT,
 director varchar(255),
 productora varchar(255)
 );
+
+# Inserts de ejemplo
+insert into Series(nombre, temporadas, valoracion, premios, director, productora) values('Breaking bad', '5', 9.9, 10000, 'Vince Gilligan', 'Sony pictures');
+insert into Series(nombre, temporadas, valoracion, premios, director, productora) values('Dark', '7', 5, 3, 'Un tío random', 'Alguna productora alemana');
+insert into Series(nombre, temporadas, valoracion, premios, director, productora) values('Stranger Things', '4', 0.0, 0, 'Shawn Levy', 'Netflix');
