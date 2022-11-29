9fb6f5c89cf2eb7e9b347b822014631ce3e77c32
Joel Felipe Díaz Carissimi <64970845+joeldiazcc@users.noreply.github.com>
2022-11-29 19:58:17 +0100

    Read BD

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+# Funcio per llegir elements de la taula
 def read():
     #conexión
     conexion = psycopg2.connect(user='postgres',
@@ -7,12 +8,12 @@ def read():
                                 host='localhost',
                                 port='5432',
                                 database='postgres')
-
+    # Declarem un cursor per realitzar operacions a la BD
     cursor=conexion.cursor()
 
     #Senténcia sql
     sqlShow = "SELECT * FROM Series;"
-
+    # Consulta a la base de dades   
     cursor.execute(sqlShow)
 
     conexion.commit()
@@ -20,7 +21,8 @@ def read():
     registro = cursor.fetchall()
 
     print(registro)
-
+    
+    # Acabem conexió
     cursor.close()
     conexion.close()
 
